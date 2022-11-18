531937512db6d20abd944e2a7bbab2776dea26af
JohanMolina-dev <jsmolina49@gmail.com>
2022-11-18 13:11:00 -0500

    cambios de configuracion en los puertos

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 
 const app = express();
 conectarDBDeseo();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/deseos", require("./routes/deseo"));
 
-app.listen(4001, () => {
-  console.log("servidor Deseos");
+app.listen(port, () => {
+  console.log("servidor Usuarios SERVER ON PORT ${app.get('port')]");
 });
